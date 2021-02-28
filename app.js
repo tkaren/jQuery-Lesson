0ee821fb0f1fdba50095318de4333f65af56413a
tsunodakaren <k6432286@gmail.com>
2021-02-28 11:35:01 +0900

    [strat] jQuery]

---
+++ app.js
@@ -0,0 +1,73 @@
+// $(function (){
+//     $('.box1').slideDown(() => {
+//             $('.box1').css({
+//                 'background-color': '#0000FF',
+//                 'width': '200px',
+//                 'height': '100px'
+//             }).slideUp();
+//         });
+// });
+
+// $(function(){
+//     $('.box1').slideDown();
+// });
+
+// $(function(){
+//     $('.box1').slideUp();
+// });
+
+// $(function(){
+//     $('.box1').show();
+//     $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//     $('.box1').hide();
+// });
+
+// $(function(){
+//     $('.box1').mouseover(function(){
+//         $('.box1').addClass('box1-ext');
+//     });
+//     $('.box1').mouseout(function(){
+//         $('.box1').removeClass('box1-ext')
+//     });
+// });
+
+// $(function(){
+//     $('.box1').on('click', function(){
+//         $('.box1').addClass('box1-ext');
+//     });
+//     $('.box1').mouseout(function(){
+//         $('.box1').removeClass('box1-ext');
+//     });
+// });
+
+// ====================7章
+// $(function(){
+//     $('.bg1').on('click', function(){
+//         $('.bg1').slideUp();
+//     });
+
+//     $('.bg2').on('click', function(){
+//         $('.bg2').slideUp();
+//     });
+
+//     $('.bg3').on('click', function(){
+//         $('.bg3').slideUp();
+//     });
+
+//     $('.bg4').on('click', function(){
+//         $('.bg4').slideUp();
+//     });
+// });
+
+// $(function(){
+//     $('.box1').on('click', function(){
+//         $(this).slideUp();
+//     });
+// });
+
+$(function(){
+    $('button').on
+})
